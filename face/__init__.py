f6bafe896e6d8a7d7fc6968aa1066093eab0cede
jwookie2 <jwookie2@hotmail.com>
2026-08-22 15:40:14 -0400

    Initialize face module with imports and exports

---
+++ face/__init__.py
@@ -0,0 +1,5 @@
+from .faceflow_bridge import FaceFlowBridge
+from .emotion_mapper import emotion_to_affect
+from .fusion import fuse_states
+
+__all__ = ["FaceFlowBridge", "emotion_to_affect", "fuse_states"]
